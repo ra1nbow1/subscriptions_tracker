a61eba1062afb2595bf03899b96f3f14588c029f
Matvey Romanov <youtuberm@yandex.ru>
2024-07-02 12:26:38 +0300

    Добавил флаг для проды

--- src/features/auth/axios.ts
+++ src/features/auth/axios.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
 const instance = axios.create({
-	baseURL: 'http://localhost:5555/',
+	baseURL:
+		process.env.ENVIRONMENT === 'development'
+			? 'http://localhost:5555/'
+			: 'https://194.32.248.209:5555/',
 	headers: {
 		'Content-Type': 'application/json',
 	},
