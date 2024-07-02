04061d63f1f9f6bc99f812bfcd31413c5dbf03fc
Matvey Romanov <youtuberm@yandex.ru>
2024-07-02 13:06:58 +0300

    Исправил URL для проды

--- src/features/auth/axios.ts
+++ src/features/auth/axios.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
 	baseURL:
 		process.env.ENVIRONMENT === 'development'
 			? 'http://localhost:5555/'
-			: 'https://194.32.248.209:5555/',
+			: 'http://194.32.248.209:5555/',
 	headers: {
 		'Access-Control-Allow-Origin': '*',
 		'Content-Type': 'application/json',
