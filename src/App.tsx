b270d5ed00acdce15d90f1361b16fc2e3da5224e
Matvey Romanov <youtuberm@yandex.ru>
2024-07-09 13:07:30 +0300

    Добавил аналитику верселя

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { setToken } from './features/auth/authSlice'
 import './index.css'
 import useTelegram from './features/hooks/useTelegram'
 import Telegram from './pages/Telegram/Telegram'
+import { Analytics } from "@vercel/analytics/react"
 
 const App: React.FC = () => {
 	const dispatch = useDispatch()
@@ -54,6 +55,7 @@ const App: React.FC = () => {
 							)
 						}
 					/>
+					<Analytics />
 				</Routes>
 			</div>
 		</Router>
