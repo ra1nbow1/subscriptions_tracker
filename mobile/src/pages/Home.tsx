c4ce9bfaa875eb58ec4c6f6eaacb68ca9b91a54a
Matvey Romanov <youtuberm@yandex.ru>
2025-09-02 10:44:18 +0300

    fix

--- mobile/src/pages/Home.tsx
+++ mobile/src/pages/Home.tsx
@@ -1,17 +1,25 @@
+import {
+	IonContent,
+	IonHeader,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+} from '@ionic/react'
 import React from 'react'
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react'
+import { Link } from 'react-router-dom'
 
 const Home: React.FC = () => (
-  <IonPage>
-    <IonHeader>
-      <IonToolbar>
-        <IonTitle>Subscriptions Tracker</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent className="ion-padding">
-      Welcome to the mobile app!
-    </IonContent>
-  </IonPage>
+	<IonPage>
+		<IonHeader>
+			<IonToolbar>
+				<IonTitle>Subscriptions Tracker</IonTitle>
+			</IonToolbar>
+		</IonHeader>
+		<IonContent className="ion-padding">
+			Welcome to the mobile app!
+			<Link to="/login">Ссылка</Link>
+		</IonContent>
+	</IonPage>
 )
 
 export default Home
